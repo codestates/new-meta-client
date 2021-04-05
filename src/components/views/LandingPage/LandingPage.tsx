54455586dfb81fbc90b1e0d145c8d4d057005498
osunguk(mac) <osunguk@gmail.com>
2021-04-05 20:23:07 +0900

    💄  스타일 : 기본 page 세팅

--- src/components/views/LandingPage/LandingPage.tsx
+++ src/components/views/LandingPage/LandingPage.tsx
@@ -1,31 +1,12 @@
 import React, { ReactElement } from "react";
 import { withRouter } from "react-router-dom";
-import svg from "../../../branding.svg";
 
 // interface Props {}
 
 function LandingPage(): ReactElement {
   return (
-    <div className="App">
-      <div className="Header">
-        <div className="logo">
-          <div>
-            <img src={svg} alt=""></img>
-          </div>
-          &nbsp;&nbsp;new-meta
-        </div>
-        <div className="title">new-meta</div>
-        <div className="button">
-          <div>Login</div>
-          <div>Sign Up</div>
-        </div>
-      </div>
-
-      <div className="Content">
-        <div>new META 에 오신걸 환영합니다!</div>
-        <div>Coming soon ...</div>
-      </div>
-      <div></div>
+    <div className="landing">
+      <h1>랜딩 페이지 입니다 </h1>
     </div>
   );
 }
