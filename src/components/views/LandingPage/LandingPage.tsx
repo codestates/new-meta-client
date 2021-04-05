06d7538393934b4c7a49e9578b338102c6cf1bfd
osunguk(mac) <osunguk@gmail.com>
2021-04-05 23:42:35 +0900

    🔨 추가 : 랜딩 페이지 마크업

--- src/components/views/LandingPage/LandingPage.tsx
+++ src/components/views/LandingPage/LandingPage.tsx
@@ -1,12 +1,62 @@
 import React, { ReactElement } from "react";
 import { withRouter } from "react-router-dom";
+// import arena from "../../../assets/image/ionia.jpeg";
+import minimap from "../../../assets/image/lol-minimap.png";
+import arena from "../../../assets/image/freljord.jpeg";
+// import arena from "../../../assets/image/arena.jpeg";
+// import arena from "../../../assets/image/arena.jpeg";
 
 // interface Props {}
 
 function LandingPage(): ReactElement {
   return (
     <div className="landing">
-      <h1>랜딩 페이지 입니다 </h1>
+      <div className="landing-img-wrapper">
+        <img className="landing-img" src={arena} alt=""></img>
+        <div className="landing-text">
+          <div className="landing-title">Welcome to New-Meta</div>
+          <div className="landing-discription">
+            검색하고 비교하고 분석하세요
+          </div>
+        </div>
+      </div>
+      <div className="landing-img-wrapper">
+        <img className="landing-img" src={minimap} alt=""></img>
+        <div className="landing-text">
+          <div className="landing-title">Welcome to New-Meta</div>
+          <div className="landing-discription">
+            검색하고 비교하고 분석하세요
+          </div>
+        </div>
+      </div>
+      {/*
+      <div className="landing-img-wrapper">
+        <img className="landing-img" src={arena} alt=""></img>
+        <div className="landing-text">
+          <div className="landing-title">Welcome to New-Meta</div>
+          <div className="landing-discription">
+            검색하고 비교하고 분석하세요
+          </div>
+        </div>
+      </div>
+      <div className="landing-img-wrapper">
+        <img className="landing-img" src={arena} alt=""></img>
+        <div className="landing-text">
+          <div className="landing-title">Welcome to New-Meta</div>
+          <div className="landing-discription">
+            검색하고 비교하고 분석하세요
+          </div>
+        </div>
+      </div>
+      <div className="landing-img-wrapper">
+        <img className="landing-img" src={arena} alt=""></img>
+        <div className="landing-text">
+          <div className="landing-title">Welcome to New-Meta</div>
+          <div className="landing-discription">
+            검색하고 비교하고 분석하세요
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
