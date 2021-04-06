cc06f6af0946b2edf49d3337c7ba2655773eb25a
osunguk(mac) <osunguk@gmail.com>
2021-04-06 23:35:39 +0900

    ✨ 추가기능 : 공략 게시판 목업 완성

--- src/components/views/BoardPage/BoardPage.tsx
+++ src/components/views/BoardPage/BoardPage.tsx
@@ -1,10 +1,44 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { ReactElement } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
+import ChampionCard from "./sections/ChampionCard";
 
 // interface Props {}
 
 function BoardPage(): ReactElement {
-  return <div>BoardPage</div>;
+  return (
+    <div className="board-page">
+      <div className="search-bar">
+        <input type="text"></input>
+        <i className="icon-search"></i>
+      </div>
+      <div className="title">챔피언 별</div>
+      <div className="champions">
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+        <ChampionCard />
+      </div>
+      <div className="title">
+        사용자 추천 메타
+        <Link to="/board/write">
+          <button className="btn-primary" type="button">
+            작성하기
+          </button>
+        </Link>
+      </div>
+      <div className="recommend"></div>
+    </div>
+  );
 }
 
 export default withRouter(BoardPage);
