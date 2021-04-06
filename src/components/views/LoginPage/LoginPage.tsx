e4e859bd534ec6f79bab16cfadb1fda5e14f4296
Yeonju Lee <jessy2yon@gmail.com>
2021-04-07 03:04:46 +0900

    ✨ 추가기능 : 로그인 모달창 추가

--- src/components/views/LoginPage/LoginPage.tsx
+++ src/components/views/LoginPage/LoginPage.tsx
@@ -1,10 +1,50 @@
 import React, { ReactElement } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 
-// interface Props {}
+interface Props extends RouteComponentProps {
+  IsModalOpen: boolean;
+  closeModal: () => void;
+}
 
-function LoginPage(): ReactElement {
-  return <div>LoginPage</div>;
+function LoginPage({ IsModalOpen, closeModal }: Props): ReactElement {
+  // const { IsModalOpen } = props;
+
+  return (
+    <>
+      {IsModalOpen ? (
+        <div className="modal-background">
+          <div className="login-box">
+            <button className="btn-close" type="button" onClick={closeModal}>
+              <i className="icon-cross"></i>
+            </button>
+            <h2>Login</h2>
+            <form>
+              <div className="user-box">
+                <input
+                  className="input-email"
+                  placeholder="Email"
+                  type="email"
+                />
+              </div>
+              <div className="user-box">
+                <input
+                  className="input-password"
+                  placeholder="Password"
+                  type="password"
+                />
+              </div>
+              <div className="btn-wrapper">
+                <button type="submit">Login</button>
+                <i className="icon-google"></i>
+                <i className="icon-facebook"></i>
+                <i className="icon-github"></i>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 }
 
 export default withRouter(LoginPage);
