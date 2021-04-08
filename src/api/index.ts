7699c282059491da855c251690494d62de5a17cb
osunguk(mac) <osunguk@gmail.com>
2021-04-08 22:12:39 +0900

    📝  문서 : api 요청 주소 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,11 @@ const API = {
   allChampionInfo:
     "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/ko_KR/champion.json",
   championInfo:
-    "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/ko_KR/champion/", // + 챔피언 영문명 ex) Bard.json
-  championSplash: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/", // + 챔피언 영문명_스킨 아이디 값 ex) Aatrox_1.jpg
+    "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/ko_KR/champion", // + 챔피언 영문명 ex) Bard.json
+  championSplash: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash", // + 챔피언 영문명_스킨 아이디 값 ex) Aatrox_1.jpg
   championLoading:
-    "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/", // ex) Aatrox_0.jpg
-  championSquare: "http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/", // Aatrox.png
+    "http://ddragon.leagueoflegends.com/cdn/img/champion/loading", // ex) Aatrox_0.jpg
+  championSquare: "http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion", // Aatrox.png
+  championSpell: "http://ddragon.leagueoflegends.com/cdn/11.7.1/img/spell", // BardQ.png
 };
 export default API;
