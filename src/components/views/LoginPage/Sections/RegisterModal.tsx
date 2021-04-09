6969904828e920c82f07d1b9b88cf4c7f71ebf35
Yeonju Lee <jessy2yon@gmail.com>
2021-04-10 02:36:31 +0900

    ✨ 추가기능 : 회원가입 기능 구현

--- src/components/views/LoginPage/Sections/RegisterModal.tsx
+++ src/components/views/LoginPage/Sections/RegisterModal.tsx
@@ -1,10 +1,51 @@
-import React, { ReactElement } from "react";
+import axios from "axios";
+import React, { ReactElement, useState } from "react";
 import { withRouter, RouteComponentProps } from "react-router-dom";
+import API from "../../../../api";
 
 interface Props extends RouteComponentProps {
   closeModal: () => void;
 }
 function RegisterModal({ closeModal }: Props): ReactElement {
+  const [Email, setEmail] = useState("");
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event: {
+    currentTarget: { value: React.SetStateAction<string> };
+  }) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onUsernameHandler = (event: {
+    currentTarget: { value: React.SetStateAction<string> };
+  }) => {
+    setUsername(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event: {
+    currentTarget: { value: React.SetStateAction<string> };
+  }) => {
+    setPassword(event.currentTarget.value);
+  };
+  const registerHandler = () => {
+    const body = {
+      email: Email,
+      nickname: Username,
+      password: Password,
+    };
+    //! 유효성 검사
+    axios
+      .post(API.user_register_test, body, { withCredentials: true })
+      .then((res) => {
+        if (res.data.user) {
+          //! 가입 성공
+          // console.log("success", res.data.user);
+        } else {
+          //! 가입 실패
+          // console.log("fail", res);
+        }
+      });
+  };
+
   return (
     <div className="modal-box">
       <button className="btn-close" type="button" onClick={closeModal}>
@@ -14,13 +55,19 @@ function RegisterModal({ closeModal }: Props): ReactElement {
       <h2>Register</h2>
       <form>
         <div className="user-box register-email">
-          <input className="input-email" placeholder="Email" type="email" />
+          <input
+            className="input-email"
+            placeholder="Email"
+            type="email"
+            onChange={onEmailHandler}
+          />
         </div>
         <div className="user-box register-username">
           <input
             className="input-username"
             placeholder="Username"
             type="text"
+            onChange={onUsernameHandler}
           />
         </div>
         <div className="user-box register-password">
@@ -28,6 +75,7 @@ function RegisterModal({ closeModal }: Props): ReactElement {
             className="input-password"
             placeholder="Password"
             type="password"
+            onChange={onPasswordHandler}
           />
         </div>
         <div className="user-box register-confirm-password">
@@ -42,6 +90,7 @@ function RegisterModal({ closeModal }: Props): ReactElement {
           <button
             type="submit"
             onClick={() => {
+              registerHandler();
               closeModal();
             }}
           >
