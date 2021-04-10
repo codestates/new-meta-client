ac2c5b399c8fca981263738fe2a9f49c2488bc8a
Yeonju Lee <jessy2yon@gmail.com>
2021-04-10 20:06:48 +0900

    💄 스타일 : Loading 컴포넌트 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,6 +10,7 @@ import BoardPage from "./views/BoardPage/BoardPage";
 import BoardReadPage from "./views/BoardReadPage/BoardReadPage";
 import BoardWritePage from "./views/BoardWritePage/BoardWritePage";
 import Footer from "./views/Footer/Footer";
+import Loading from "./utils/Loading";
 
 function App(): ReactElement {
   return (
@@ -24,6 +25,7 @@ function App(): ReactElement {
           <Route exact path="/board" component={BoardPage} />
           <Route exact path="/board/read" component={BoardReadPage} />
           <Route exact path="/board/write" component={BoardWritePage} />
+          <Route exact path="/loading" component={Loading} />
         </Switch>
       </div>
       <Footer />
