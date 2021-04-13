d2971dfab5f903f802e55ff7363f130fb50a37e0
osunguk(mac) <osunguk@gmail.com>
2021-04-14 04:38:49 +0900

    💄  스타일 : empty board 정렬 수정

--- src/components/views/BoardPage/sections/EmptyDetail.tsx
+++ src/components/views/BoardPage/sections/EmptyDetail.tsx
@@ -5,9 +5,11 @@ function EmptyDetail(): ReactElement {
   return (
     <>
       <div className="text-box">
-        <div className="text-box-title">Click Post</div>
-        <div className="text-box-description">
-          Check out other people&apos;s New Meta.
+        <div className="empty-detail-board">
+          <div className="text-box-title">Click Post</div>
+          <div className="text-box-description">
+            Check out other people&apos;s New Meta.
+          </div>
         </div>
       </div>
       <img src={demacia} alt=""></img>
