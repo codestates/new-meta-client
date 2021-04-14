c4e9dfbb7a9e2ab909ee91991ef3fb405bea75ab
osunguk(mac) <osunguk@gmail.com>
2021-04-14 14:24:33 +0900

    🐞  버그 : 게시물 스왑시 생기던 오류 수정

--- src/components/views/BoardPage/sections/BoardDetail.tsx
+++ src/components/views/BoardPage/sections/BoardDetail.tsx
@@ -12,9 +12,11 @@ interface Props {
 
 function BoardDetail(props: Props): ReactElement {
   const { data } = props;
-  const [content, setcontent] = useState<any[]>([]);
-  const [CurrentIndex, setCurrentIndex] = useState(0);
+  // console.log("??", data);
 
+  const [viewData, setdata] = useState(data);
+  const [CurrentIndex, setCurrentIndex] = useState(0);
+  const [info, setinfo] = useState<any>(null);
   const textTag = useRef<HTMLDivElement>(null);
   const partTag = useRef<HTMLDivElement>(null);
 
@@ -24,12 +26,12 @@ function BoardDetail(props: Props): ReactElement {
     }
     textTag.current?.children[CurrentIndex].classList.remove("is-active");
     textTag.current?.children[CurrentIndex - 1].classList.add("is-active");
-    const data = document.querySelector(".text-box");
+    const box = document.querySelector(".text-box");
     // eslint-disable-next-line @typescript-eslint/no-inferrable-types
     let textBoxWidth: string = "0px";
-    if (data) {
-      textBoxWidth = window.getComputedStyle(data).width;
-      console.log(window.getComputedStyle(data).width);
+    if (box) {
+      textBoxWidth = window.getComputedStyle(box).width;
+      console.log(window.getComputedStyle(box).width);
     }
     const tt = textTag.current?.style;
     const pt = partTag.current?.style;
@@ -52,7 +54,6 @@ function BoardDetail(props: Props): ReactElement {
     let textBoxWidth: string = "0px";
     if (data) {
       textBoxWidth = window.getComputedStyle(data).width;
-      console.log(window.getComputedStyle(data).width);
     }
     const tt = textTag.current?.style;
     const pt = partTag.current?.style;
@@ -65,55 +66,27 @@ function BoardDetail(props: Props): ReactElement {
   };
 
   useEffect(() => {
+    textTag.current?.children[1].classList.remove("is-active");
+    textTag.current?.children[2].classList.remove("is-active");
+    textTag.current?.children[3].classList.remove("is-active");
+    textTag.current?.children[0].classList.add("is-active");
+    const tt = textTag.current?.style;
+    if (tt) {
+      tt.transform = `translateX(0px)`;
+    }
+    setdata(data);
     const run = async () => {
       const champInfo = await axios.get(
         `${API.championInfo}/${data.champion}.json`
       );
       const info = champInfo.data.data[data.champion];
-
-      const contentsArr = [
-        <div ref={partTag} className="part part1 is-active">
-          <div className="contents-title">{data.title}</div>
-          <div className="contents-description">{data.description}</div>
-        </div>,
-        <div className="part part2">
-          <div className="contents-skill">
-            {info.spells.map((el: any, idx: number) => {
-              return (
-                <div className="skill-group" key={idx}>
-                  <img
-                    src={`${API.championSpell}/${el.image.full}`}
-                    alt=""
-                  ></img>
-                  <div>{data.skills[idx]}</div>
-                </div>
-              );
-              //
-            })}
-          </div>
-        </div>,
-        <div className="part part3">
-          <div className="label">플레이 할 때</div>
-          <div className="play">{data.play[0]}</div>
-          <div className="label">상대 할 때</div>
-          <div className="enemy">{data.play[1]}</div>
-        </div>,
-        <div className="part part4">
-          <div className="label">꿀 팁</div>
-          <div className="etc">{data.etc}</div>
-          <div className="button-group">
-            <i className="icon-user"></i>
-            {data.author}
-            <i className="icon-star-full"></i>
-            <i className="icon-star-empty"></i>
-            <i className="icon-file"></i>
-          </div>
-        </div>,
-      ];
-      setcontent(contentsArr);
+      setinfo(info);
+      // const contentsArr = [];
+      // setcontent(contentsArr);
+      setCurrentIndex(0);
     };
     run();
-  }, []);
+  }, [data]);
 
   return (
     <>
@@ -130,14 +103,44 @@ function BoardDetail(props: Props): ReactElement {
         ></i>
       </div>
       <div ref={textTag} className="text-box">
-        {content.length > 0 && (
-          // <>{content[0]}</>
-          <>
-            {content.map((el) => {
-              return el;
-            })}
-          </>
-        )}
+        <div ref={partTag} className="part part1 is-active">
+          <div className="contents-title">{viewData.title}</div>
+          <div className="contents-description">{viewData.description}</div>
+        </div>
+        <div className="part part2">
+          <div className="contents-skill">
+            {info &&
+              info.spells.map((el: any, idx: number) => {
+                return (
+                  <div className="skill-group" key={idx}>
+                    <img
+                      src={`${API.championSpell}/${el.image.full}`}
+                      alt=""
+                    ></img>
+                    <div>{viewData.skills[idx]}</div>
+                  </div>
+                );
+                //
+              })}
+          </div>
+        </div>
+        <div className="part part3">
+          <div className="label">플레이 할 때</div>
+          <div className="play">{viewData.play[0]}</div>
+          <div className="label">상대 할 때</div>
+          <div className="enemy">{viewData.play[1]}</div>
+        </div>
+        <div className="part part4">
+          <div className="label">꿀 팁</div>
+          <div className="etc">{viewData.etc}</div>
+          <div className="button-group">
+            <i className="icon-user"></i>
+            {viewData.author}
+            <i className="icon-star-full"></i>
+            <i className="icon-star-empty"></i>
+            <i className="icon-file"></i>
+          </div>
+        </div>
       </div>
       <img
         className="detail-img"
