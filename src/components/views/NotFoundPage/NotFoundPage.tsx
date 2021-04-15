24c98195431618f41ba4632cb1c233d1875eeedc
osunguk(mac) <osunguk@gmail.com>
2021-04-15 17:03:09 +0900

    💄  스타일 : not found 디자인 완료

--- src/components/views/NotFoundPage/NotFoundPage.tsx
+++ src/components/views/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,18 @@
 import React, { ReactElement } from "react";
+import targon from "../../../assets/image/targon.jpeg";
 
 function NotFoundPage(): ReactElement {
-  return <div>not found</div>;
+  return (
+    <div className="not-found">
+      <img src={targon} alt=""></img>
+      <div className="error">
+        <div className="hit-the-floor">404</div>
+        <div className="hit-the-floor text">error</div>
+      </div>
+      <div className="explain">페이지를 찾을 수 없습니다</div>
+      <a href="/">메인으로 가기</a>
+    </div>
+  );
 }
 
 export default NotFoundPage;
