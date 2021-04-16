0a9d1c28f642c6ea83b331e2be63584787308ece
nahsooyeon <nahsooyeon@gmail.com>
2021-04-16 20:46:52 +0900

    🔧  수정: 롤 IP 요청 주소 수정 (패치버젼 11.8.1)

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,12 +16,12 @@ const API = {
   user_profile_test: `${domain}/users/me`,
   user_update_test: `${domain}/users/me`,
   user_delete_test: `${domain}/users/me`,
-  summonerInfo: `${domain}/summoners/summonerInfo`,
+  summonerInfo: `${domain}summoners/summonerInfo`,
 
   allChampionInfo:
-    "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/ko_KR/champion.json",
+    "http://ddragon.leagueoflegends.com/cdn/11.8.1/data/ko_KR/champion.json",
   championInfo:
-    "http://ddragon.leagueoflegends.com/cdn/11.7.1/data/ko_KR/champion", // + 챔피언 영문명 ex) Bard.json
+    "http://ddragon.leagueoflegends.com/cdn/11.8.1/data/ko_KR/champion", // + 챔피언 영문명 ex) Bard.json
   championSplash: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash", // + 챔피언 영문명_스킨 아이디 값 ex) Aatrox_1.jpg
   championLoading:
     "http://ddragon.leagueoflegends.com/cdn/img/champion/loading", // ex) Aatrox_0.jpg
