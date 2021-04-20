40fc99f0b724cc3439960e5a2e831529b5e53912
osunguk(mac) <osunguk@gmail.com>
2021-04-20 13:42:57 +0900

    📝  문서 : 11.8.1 패치 적용

--- src/api/index.ts
+++ src/api/index.ts
@@ -25,7 +25,7 @@ const API = {
   championSplash: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash", // + 챔피언 영문명_스킨 아이디 값 ex) Aatrox_1.jpg
   championLoading:
     "http://ddragon.leagueoflegends.com/cdn/img/champion/loading", // ex) Aatrox_0.jpg
-  championSquare: "http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion", // Aatrox.png
-  championSpell: "http://ddragon.leagueoflegends.com/cdn/11.7.1/img/spell", // BardQ.png
+  championSquare: "http://ddragon.leagueoflegends.com/cdn/11.8.1/img/champion", // Aatrox.png
+  championSpell: "http://ddragon.leagueoflegends.com/cdn/11.8.1/img/spell", // BardQ.png
 };
 export default API;
