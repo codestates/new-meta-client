8000697099d5fb10d96ce2ef376e5f4a8f473cd3
nahsooyeon <nahsooyeon@gmail.com>
2021-04-21 02:33:30 +0900

    🔨 수정: 솔로/듀오 검색 페이지 컴포넌트로 분리

---
+++ src/components/views/PlayersSearchPage/DuoMatchView.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import {
+  SummonerAllData,
+  MatchInfo,
+  LeagueInfo,
+  LaneInfo,
+  PlayerMatchInfo,
+  FrameExpData,
+  ParticipantFrames,
+  Position,
+  KDAEventData,
+} from "./interface";
+import lanerData from "./lanerData.json";
+import junglerData from "./junglerData.json";
+import LaneInfoChart from "./Sections/LaneInfoChart";
+import HeatMapChart from "./Sections/HeatMapChart";
+import TimelineChart from "./Sections/TimelineChart";
+import WinRateChart from "./Sections/WinRateChart";
+import TagComponent from "./Sections/TagComponent";
+import MostChampion from "./Sections/MostChampion";
+import MatchingPoints from "./Sections/MatchingPoints";
+
+interface Props {
+  User1data: SummonerAllData;
+  User2data: SummonerAllData;
+}
+
+function DuoMatchView(props: Props) {
+  const { User1data, User2data } = props;
+
+  return (
+    <div className="duo-search-result">
+      <div className="duo-match-thumbnail">
+        <div className="match-success-point">
+          <MatchingPoints User1data={User1data} User2data={User2data} />
+        </div>
+        <div className="two-summoner-info">
+          <div className="summonerInfo">
+            <div className="summoner-name">
+              <div>{User1data.leagueInfo!.summonerName}</div>
+            </div>
+            <div className="summoner-lank-info">
+              <div className="summoner-tier">
+                {User1data.leagueInfo!.tier} {User1data.leagueInfo!.rank}
+              </div>
+              <div className="summoner-tier">
+                {User1data.leagueInfo!.leaguePoints} LP
+              </div>
+              <div className="summoner-tags">
+                <TagComponent
+                  laneInfo={User1data.laneInfo!}
+                  leagueInfo={User1data.leagueInfo!}
+                  kdaInfo={User1data.kdaTimelineData!}
+                  recentChampionStats={User1data.recentChampionStats!}
+                />
+              </div>
+              <div className="summoner-most-champion">
+                <MostChampion
+                  userData={User1data.recentChampionStats!}
+                  idx={0}
+                />
+                <MostChampion
+                  userData={User1data.recentChampionStats!}
+                  idx={1}
+                />
+                <MostChampion
+                  userData={User1data.recentChampionStats!}
+                  idx={2}
+                />
+              </div>
+              <div className="summoner-graph">
+                <div className="graph-section">
+                  <div className="graph">
+                    <WinRateChart userData={User1data.leagueInfo!} />
+                  </div>
+                  <div className="graph">
+                    <LaneInfoChart userData={User1data.laneInfo!} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="summonerInfo">
+            <div className="summoner-name">
+              <div>{User2data.leagueInfo!.summonerName}</div>
+            </div>
+            <div className="summoner-lank-info">
+              <div className="summoner-tier">
+                {User2data.leagueInfo!.tier} {User2data.leagueInfo!.rank}
+              </div>
+              <div className="summoner-tier">
+                {User2data.leagueInfo!.leaguePoints} LP
+              </div>
+              <div className="summoner-tags">
+                <TagComponent
+                  laneInfo={User2data.laneInfo!}
+                  leagueInfo={User2data.leagueInfo!}
+                  kdaInfo={User2data.kdaTimelineData!}
+                  recentChampionStats={User2data.recentChampionStats!}
+                />
+              </div>
+              <div className="summoner-most-champion">
+                <MostChampion
+                  userData={User1data.recentChampionStats!}
+                  idx={0}
+                />
+                <MostChampion
+                  userData={User1data.recentChampionStats!}
+                  idx={1}
+                />
+                <MostChampion
+                  userData={User1data.recentChampionStats!}
+                  idx={2}
+                />
+              </div>
+              <div className="summoner-graph">
+                <div className="graph-section">
+                  <div className="graph">
+                    <WinRateChart userData={User2data.leagueInfo!} />
+                  </div>
+                  <div className="graph">
+                    <LaneInfoChart userData={User2data.laneInfo!} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DuoMatchView;
---
+++ src/components/views/PlayersSearchPage/SoloMatchView.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable import/no-unresolved */
+import React, { ReactElement, useState, useEffect } from "react";
+import Challenger from "../../../assets/image/emblem/Challenger.png";
+import {
+  SummonerAllData,
+  MatchInfo,
+  LeagueInfo,
+  LaneInfo,
+  PlayerMatchInfo,
+  FrameExpData,
+  ParticipantFrames,
+  Position,
+  KDAEventData,
+} from "./interface";
+
+import lanerData from "./lanerData.json";
+import junglerData from "./junglerData.json";
+
+import LaneInfoChart from "./Sections/LaneInfoChart";
+import HeatMapChart from "./Sections/HeatMapChart";
+import TimelineChart from "./Sections/TimelineChart";
+import WinRateChart from "./Sections/WinRateChart";
+import TagComponent from "./Sections/TagComponent";
+import MostChampion from "./Sections/MostChampion";
+import MatchingPoints from "./Sections/MatchingPoints";
+
+interface Props {
+  User1data: SummonerAllData;
+}
+
+function SoloMatchView(props: Props): ReactElement {
+  const { User1data } = props;
+  return (
+    <div className="solo-search-result">
+      <div className="summonerInfo">
+        <div className="summoner-name">
+          <div>{User1data.leagueInfo!.summonerName}</div>
+        </div>
+        <div className="summoner-lank-info">
+          <div className="summoner-tier">
+            {User1data.leagueInfo!.tier} {User1data.leagueInfo!.rank}
+          </div>
+          <div className="summoner-tier">
+            {User1data.leagueInfo!.leaguePoints} LP
+          </div>
+        </div>
+        <div className="summoner-tags">
+          <TagComponent
+            laneInfo={User1data.laneInfo!}
+            leagueInfo={User1data.leagueInfo!}
+            kdaInfo={User1data.kdaTimelineData!}
+            recentChampionStats={User1data.recentChampionStats!}
+          />
+        </div>
+      </div>
+      <div className="summoner-graph">
+        <div className="graph-section">
+          <div className="graph win-rate">
+            <WinRateChart userData={User1data.leagueInfo!} />
+          </div>
+          {/* <div className="graph">
+            <LaneInfoChart userData={User1data.laneInfo!} />
+          </div> */}
+          <div className="summoner-most-champion">
+            <div className="mostChamp">
+              <MostChampion userData={User1data.recentChampionStats!} idx={0} />
+            </div>
+            <div className="mostChamp">
+              <MostChampion userData={User1data.recentChampionStats!} idx={1} />
+            </div>
+            <div className="mostChamp">
+              <MostChampion userData={User1data.recentChampionStats!} idx={2} />
+            </div>
+          </div>
+        </div>
+        <div className="graph-section">
+          <div className="graph exp-timeline">
+            <TimelineChart userData={User1data.expTimelineData!} />
+          </div>
+          <div className="graph">
+            <HeatMapChart userData={User1data.recentChampionStats!} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SoloMatchView;
