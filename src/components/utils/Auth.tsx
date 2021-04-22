9ead550f48cc7413b86f6e3ea8c55c42fbb9d94d
Yeonju Lee <jessy2yon@gmail.com>
2021-04-22 14:34:03 +0900

    ✨ 추가기능 : 접근권한 없는 페이지 접근시 팝업창 띄우는 기능 추가

--- src/components/utils/Auth.tsx
+++ src/components/utils/Auth.tsx
@@ -1,18 +1,31 @@
-import React, { ComponentType, ReactElement, useEffect } from "react";
+import React, { ComponentType, ReactElement, useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_CURRENT_USER } from "../../graphql";
+import Popup from "./Popup";
 
 export default (SpecificComponent: ComponentType, option: boolean) => {
   function Auth(props: any): ReactElement {
     const currentUser = useQuery(GET_CURRENT_USER);
+    const [IsPopupOpen, setIsPopupOpen] = useState(false);
+    const popupMessage = "로그인이 필요한 페이지입니다.";
+    const btnMessage = "메인으로 돌아가기";
 
     useEffect(() => {
       if (!currentUser.data.token && option) {
-        props.history.push("/");
+        setIsPopupOpen(true);
       }
-    }, [currentUser, props]);
+    }, [IsPopupOpen, currentUser, props]);
 
-    return <SpecificComponent />;
+    return IsPopupOpen ? (
+      <Popup
+        popupMessage={popupMessage}
+        btnMessage={btnMessage}
+        IsPopupOpen={IsPopupOpen}
+        setIsPopupOpen={setIsPopupOpen}
+      />
+    ) : (
+      <SpecificComponent />
+    );
   }
   return Auth;
 };
