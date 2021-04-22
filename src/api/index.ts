cf8b8db8624eca6ee16d91bd615e069236444c4a
Yeonju Lee <jessy2yon@gmail.com>
2021-04-23 03:43:09 +0900

    ⚒  수정 : API url 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,13 +19,13 @@ const API = {
   summonerInfo: `${domain}/summoners/summonerInfo`,
 
   allChampionInfo:
-    "http://ddragon.leagueoflegends.com/cdn/11.8.1/data/ko_KR/champion.json",
+    "https://ddragon.leagueoflegends.com/cdn/11.8.1/data/ko_KR/champion.json",
   championInfo:
-    "http://ddragon.leagueoflegends.com/cdn/11.8.1/data/ko_KR/champion", // + 챔피언 영문명 ex) Bard.json
+    "https://ddragon.leagueoflegends.com/cdn/11.8.1/data/ko_KR/champion", // + 챔피언 영문명 ex) Bard.json
   championSplash: "http://ddragon.leagueoflegends.com/cdn/img/champion/splash", // + 챔피언 영문명_스킨 아이디 값 ex) Aatrox_1.jpg
   championLoading:
-    "http://ddragon.leagueoflegends.com/cdn/img/champion/loading", // ex) Aatrox_0.jpg
-  championSquare: "http://ddragon.leagueoflegends.com/cdn/11.8.1/img/champion", // Aatrox.png
-  championSpell: "http://ddragon.leagueoflegends.com/cdn/11.8.1/img/spell", // BardQ.png
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/loading", // ex) Aatrox_0.jpg
+  championSquare: "https://ddragon.leagueoflegends.com/cdn/11.8.1/img/champion", // Aatrox.png
+  championSpell: "https://ddragon.leagueoflegends.com/cdn/11.8.1/img/spell", // BardQ.png
 };
 export default API;
